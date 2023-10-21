bef26c3e74652d13a86c4934e28538d25b92a9d0
fabricio <fabricio-fjc@outlook.com>
2023-10-21 20:43:20 -0300

    App de calculadora funcionando react-native [terminado]

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,115 @@
 import React, {useState, Component} from 'react'
-import {View, SafeAreaView, Text, StyleSheet} from 'react-native'
+import {View, SafeAreaView, Text, StyleSheet, StatusBar} from 'react-native'
 import Button from './components/Button'
 import Display from './components/Display'
 
+
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
+
 class App extends Component{
-  state = {
-    displayValue: '0'
-  }
+  state = {...initialState}
 
   addDigit = n =>{
-    this.setState({displayValue: n})
+    if(n === '.' &&this.state.displayValue.includes('.')){
+      return
+    }
+
+    const clearDisplay =this.state.displayValue === '0' ||this.state.clearDisplay
+    const currentValue = clearDisplay?'':this.state.displayValue
+    const displayValue = currentValue + n
+   this.setState({ displayValue , clearDisplay : false})
+
+    if(n !== '.'){
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+     this.setState({values})
+    }
   }
 
   clearMemory = () =>{
-    this.setState({displayValue: '0'})
+   this.setState({...initialState})
   }
 
   setOperation = op =>{
-    this.setState({displayValue: op})
+    if(this.state.current === 0){
+      this.setState({operation: op, current: 1, clearDisplay: true})
+    }else{
+      const equals = op === '='
+      const values = [...this.state.values]
+      try{
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      }catch (e){
+        values[0] = this.state.values[0]
+      }
+      values[1] = 0
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : op,
+        current: equals ? 0 : 1,
+        clearDisplay: true,
+        values
+
+      })
+    }
   }
 
   result = () =>{
     aux = eval(displayValue)
-    this.setState({displayValue: aux})
+   this.setState({displayValue: aux})
   }
 
   render = () => (
-    <SafeAreaView>
-      <View style={this.estilo.stBar}>
-        <Text style={this.estilo.barText}>Calculadora</Text>
-      </View>
-      <View style={this.estilo.container}>
-        <Display value={this.state.displayValue}></Display>
-        <View style={this.estilo.buttons}>
-          <Button label="AC" triple onclick={this.clearMemory}/>
-          <Button label="/" op onclick={this.setOperation}/>
-          <Button label="7" onclick={ this.addDigit}/>
-          <Button label="8" onclick={ this.addDigit}/>
-          <Button label="9" onclick={ this.addDigit}/>
-          <Button label="*" op onclick={this.setOperation}/>
-          <Button label="4" onclick={ this.addDigit}/>
-          <Button label="5" onclick={ this.addDigit}/>
-          <Button label="6" onclick={ this.addDigit}/>
-          <Button label="+" op onclick={this.setOperation}/>
-          <Button label="1" onclick={ this.addDigit}/>
-          <Button label="2" onclick={ this.addDigit}/>
-          <Button label="3" onclick={ this.addDigit}/>
-          <Button label="-" op onclick={this.setOperation}/>
-          <Button label="0" onclick={ this.addDigit}/>
-          <Button label="=" triple/>
+    <>
+      <StatusBar backgroundColor="#000000b3"></StatusBar>
+      <SafeAreaView>
+        <View style={this.estilo.stBar}>
+          <Text style={this.estilo.barText}>Calculadora</Text>
         </View>
-      </View>
-    </SafeAreaView>
+        <View style={this.estilo.container}>
+          <Display value={this.state.displayValue}></Display>
+          <View style={this.estilo.buttons}>
+            <Button label="AC" triple onclick={this.clearMemory}/>
+            <Button label="/" op onclick={this.setOperation}/>
+            <Button label="7" onclick={this.addDigit}/>
+            <Button label="8" onclick={this.addDigit}/>
+            <Button label="9" onclick={this.addDigit}/>
+            <Button label="*" op onclick={this.setOperation}/>
+            <Button label="4" onclick={this.addDigit}/>
+            <Button label="5" onclick={this.addDigit}/>
+            <Button label="6" onclick={this.addDigit}/>
+            <Button label="+" op onclick={this.setOperation}/>
+            <Button label="1" onclick={this.addDigit}/>
+            <Button label="2" onclick={this.addDigit}/>
+            <Button label="3" onclick={this.addDigit}/>
+            <Button label="-" op onclick={this.setOperation}/>
+            <Button label="0" double onclick={this.addDigit}/>
+            <Button label="." onclick={this.addDigit}/>
+            <Button label="=" op onclick={this.setOperation}/>
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
   )
 
   estilo = StyleSheet.create({
     stBar:{
       height: 60,
       width: '100%',
-      backgroundColor: '#a6e7ff',
+      backgroundColor: '#000000b3',
       flexDirection: 'row',
       justifyContent: 'center',
       alignItems: 'center'
     },
 
     barText:{
-      color: '#000',
+      color: '#fff',
       fontWeight: 'bold',
       fontSize: 20
     },
